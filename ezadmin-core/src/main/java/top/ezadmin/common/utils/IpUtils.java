b96295426f91b15ecc62f2e72e0acbf9270ccfca
hank <ezadmin@126.com>
2024-03-09 16:45:26 +0800

    1

--- ezadmin-core/src/main/java/top/ezadmin/common/utils/IpUtils.java
+++ ezadmin-core/src/main/java/top/ezadmin/common/utils/IpUtils.java
@@ -5,48 +5,41 @@ import java.util.regex.Pattern;
 
 
 public class IpUtils {
+    private static final String IPV4_REGEX = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+    private static final String IPV6_REGEX = "^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4})$";
+    private static final Pattern IPV4PATTERN = Pattern.compile(IPV4_REGEX);
+    private static final Pattern IPV6PATTERN = Pattern.compile(IPV6_REGEX);
+    private static final String X_FORWARDED_FOR = "x-forwarded-for";
+    private static final String X_Real_IP = "X-Real-IP";
+    private static final String PROXY_CLIENT_IP = "Proxy-Client-IP";
+    private static final String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
 
-    public final static Pattern IP_V4_PATTERN = Pattern.compile("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$");
-
-
-    /**
-     * 从请求中获取真实的IP地址<br>
-     * 优先级为：X-Forwarded-For > Proxy-Client-IP > WL-Proxy-Client-IP > HTTP_CLIENT_IP
-     * > HTTP_X_FORWARDED_FOR > RemoteAddr
-     *
-     * @param request Http请求
-     * @return true/false
-
-     * @since 2012-9-7
-     */
+    public static boolean isIpUnknown(String ip) {
+        return StringUtils.isBlank(ip) || (!IPV4PATTERN.matcher(ip).matches() && !IPV6PATTERN.matcher(ip).matches());
+    }
 
     public static String getRealIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Real-IP");
+        String ip = cut(request.getHeader(X_Real_IP));
         if (isIpUnknown(ip)) {
-            ip = request.getHeader("X-Forwarded-For");
+          ip = cut(request.getHeader(X_FORWARDED_FOR));
         }
         if (isIpUnknown(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
+            ip = cut(request.getHeader(PROXY_CLIENT_IP));
         }
         if (isIpUnknown(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (isIpUnknown(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (isIpUnknown(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (isIpUnknown(ip)) {
-            ip = request.getRemoteAddr();
-        } else {
-            return ip;
+            ip = cut(request.getHeader(WL_PROXY_CLIENT_IP));
         }
         return ip;
     }
 
-    public static boolean isIpUnknown(String ip) {
-        return StringUtils.isBlank(ip) || !IP_V4_PATTERN.matcher(ip).matches() || "unknown".equalsIgnoreCase(ip);
+    private static String cut(String ip) {
+        if (StringUtils.isBlank(ip)) {
+            return ip;
+        }
+        String str = ",";
+        if (ip.indexOf(str) > 0) {
+            ip = ip.substring(0, ip.indexOf(str));
+        }
+        return ip;
     }
-
 }
