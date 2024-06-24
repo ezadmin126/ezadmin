68db5fe20bcea3a79bd2c5df5cf3da98cdaa6487
hank <ezadmin@126.com>
2024-06-24 18:41:31 +0800

    1

--- ezadmin-web/src/main/resources/static/blog/plugins/search/search.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#searchbox').keypress(function (e) {
-        var key = e.which; //e.which是按键的值
-        if (key == 13) {
-            $("#searchForm").submit();
-        }
-    });
-});
