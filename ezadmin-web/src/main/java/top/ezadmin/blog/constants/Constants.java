25026b0bd34163fd48a50a9021ef50d5bc16e521
ezadmin <ezadmin@126.com>
2025-01-12 21:43:29 +0800

    init

--- ezadmin-web/src/main/java/top/ezadmin/blog/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package top.ezadmin.blog.constants;
-
-/**
- * : Constants
- */
-public class Constants {
-    public static final String EZ_SID = "EZ_SID";
-    public static final String SID = "SID";
-}
--- ezadmin-web/src/main/java/top/ezadmin/blog/vo/BlogMessageVO.java
+++
@@ -1,7 +0,0 @@
-package top.ezadmin.blog.vo;
-
-
-import top.ezadmin.domain.model.BlogMessage;
-
-public class BlogMessageVO extends BlogMessage {
-}
